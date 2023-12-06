261dfa6e63d025a5ac38e63ac9bf03cfbd04eec8
Obydennov Dmitrii <dima-obydennov@mail.ru>
2023-12-06 19:18:07 +0300

    m

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,12 +1,28 @@
 package hexlet.code;
 
 import io.javalin.Javalin;
+import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
+@Slf4j
 public class App {
-    public static Javalin getApp() {
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.valueOf(port);
+    }
+
+    public static void main(String[] args) throws IOException, SQLException {
+        var app = getApp();
+        app.start(getPort());
+    }
+
+    public static Javalin getApp() throws IOException, SQLException {
         var app = Javalin.create(config -> {
             config.plugins.enableDevLogging();
         });
+        app.get("/", ctx -> ctx.result("Hello World"));
         return app;
     }
 }
